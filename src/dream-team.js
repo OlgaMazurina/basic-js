ec5c4c692106f4c569f84c102dfb07b785108618
Olga Mazurina <olya_28_94@mail.ru>
2020-10-19 19:32:21 +0300

    feat: Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+module.exports = function createDreamTeam(members) {
+  let team = '';
+  let arr = [];
+
+    if (members == null || Array.isArray(members) == false) {
+      return false;
+    }
+
+  for (let i = 0; i < members.length; i++ ) {
+    if (typeof(members[i]) == 'string') {
+      for (let j = 0; j < members[i].length; j++ ) {
+        if (members[i][j] !== ' ') {
+        arr.push(members[i][j].toUpperCase());
+        break;
+        }
+      }
+    }
+
+  }
+  arr.sort();
+  team = arr.join('');
+
+  return team;
+}

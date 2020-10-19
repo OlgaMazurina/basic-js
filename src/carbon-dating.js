2a7c1967df7a88d36829f2b6a42aa24dc856f250
Olga Mazurina <olya_28_94@mail.ru>
2020-10-19 18:49:34 +0300

    feat: Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
-const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  // let numb = 0;
+
+  if (typeof(+sampleActivity) !== 'number' || sampleActivity == null || typeof(+sampleActivity) !== 'string') {
+    return false;
+  }
+
+  let k = 0.693/HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log(15/sampleActivity)/k);
+
+
+
+
+  return t;
 };

6f3540f31de74dc028b35a9051d9d2f3d3365b11
Olga Mazurina <olya_28_94@mail.ru>
2020-10-20 15:52:43 +0300

    feat: Hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+  let min = 2**disksNumber - 1; //минимально количество перемещений
+  let time = turnsSpeed/3600;  //количество перемещений за 1 с
+  let totalTime = Math.floor(min/time);
+
+
+  return { turns: min, seconds: totalTime };
+
 };

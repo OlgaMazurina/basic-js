81ca693ade4a3be71260ee164f8d3ccf998d1038
Olga Mazurina <olya_28_94@mail.ru>
2020-10-21 20:34:15 +0300

    feat: Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,68 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let total = "";
+  let addition = "";
+  let additionSeparator;
+  let separator;
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (str !== null) {
+    str = str.toString();
+  } else {
+    str = "null";
+  }
+
+  if (options["addition"] !== undefined) {
+    if (options["addition"] !== null) {
+      addition = options["addition"].toString();
+    } else {
+      addition = "null";
+    }
+
+    if (options["additionRepeatTimes"] !== undefined) {
+      if (options["additionSeparator"] !== undefined) {
+        additionSeparator = options["additionSeparator"];
+        addition = addition.concat(additionSeparator);
+        addition = addition.repeat(options["additionRepeatTimes"]);
+        addition = addition.substring(
+          0,
+          addition.length - additionSeparator.length
+        );
+      } else {
+        additionSeparator = "|";
+        addition = addition.concat(additionSeparator);
+
+        addition = addition.repeat(options["additionRepeatTimes"]);
+        addition = addition.substring(
+          0,
+          addition.length - additionSeparator.length
+        );
+      }
+    }
+    total = str.concat(addition);
+  }
+
+  if (total !== "") {
+    if (options["repeatTimes"] !== undefined) {
+      if (options["separator"] !== undefined) {
+        separator = options["separator"];
+      } else {
+        separator = "+";
+      }
+      total = total.concat(separator);
+      total = total.repeat(options["repeatTimes"]);
+      total = total.substring(0, total.length - separator.length);
+    }
+  } else {
+    if (options["repeatTimes"] !== undefined) {
+      if (options["separator"] !== undefined) {
+        separator = options["separator"];
+      } else {
+        separator = "+";
+      }
+      total = str.concat(separator);
+      total = total.repeat(options["repeatTimes"]);
+      total = total.substring(0, total.length - separator.length);
+    }
+  }
+
+  return total;
 };
-  

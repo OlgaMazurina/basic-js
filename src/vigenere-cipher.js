42fc64585c8070afff16d4dc1dbb0abcfa96b3d2
Olga Mazurina <olya_28_94@mail.ru>
2020-10-22 00:01:11 +0300

    feat: Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,89 @@
-const CustomError = require("../extensions/custom-error");
+
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+
+  constructor(bool) {
+    if (bool == true || bool == null) {
+       this.directMachine = true;
+    } else {
+      this.directMachine = false;
+    }
+
   }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (message == null || key == null) {
+      throw Error;
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+      while (message.length > key.length) {
+        key += key;
+      }
+
+      if (key.length > message.length) {
+        key = key.substring(0, message.length);
+      }
+
+      let messageLetterCode;
+      let keyLetterCode;
+      let lettersNumber = 26;
+      let codeLetter;
+      let encryptedMessage = '';
+      for (let i = 0; i < message.length; i++) {
+        messageLetterCode = message.charCodeAt(i);
+        if (messageLetterCode < 65 || messageLetterCode > 90) {
+          encryptedMessage += message[i];
+        } else {
+        keyLetterCode = key.charCodeAt(i);
+        codeLetter = (messageLetterCode + keyLetterCode) % lettersNumber;
+        encryptedMessage += String.fromCodePoint(codeLetter + 65);
+        }
+      }
+      if (!this.directMachine) {
+        encryptedMessage = encryptedMessage.split('').reverse().join('');
+
+      }
+      return encryptedMessage;
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage == null || key == null) {
+      throw Error;
+    }
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    while (encryptedMessage.length > key.length) {
+      key += key;
+    }
+
+    if (key.length > encryptedMessage.length) {
+      key = key.substring(0, encryptedMessage.length);
+    }
+    let encryptedMessageLetterCode;
+    let keyLetterCode;
+    let lettersNumber = 26;
+    let messageLetter;
+    let decryptedMessage = '';
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      encryptedMessageLetterCode = encryptedMessage.charCodeAt(i);
+      if (encryptedMessageLetterCode < 65 || encryptedMessageLetterCode > 90) {
+        decryptedMessage += encryptedMessage[i];
+      } else {
+        keyLetterCode = key.charCodeAt(i);
+        messageLetter = (encryptedMessageLetterCode + lettersNumber - keyLetterCode) % lettersNumber;
+        decryptedMessage += String.fromCodePoint(messageLetter + 65);
+      }
+
+    }
+    if (!this.directMachine) {
+      decryptedMessage = decryptedMessage.split('').reverse().join('');
+
+    }
+    return decryptedMessage;
+
   }
 }
 
